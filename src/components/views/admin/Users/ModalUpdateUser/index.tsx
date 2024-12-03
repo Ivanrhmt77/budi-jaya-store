e544dc6b7a7e637387a7abfff020c3ae9cc0931c
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2024-12-03 20:32:13 +0700

    Feat: User Management

---
+++ src/components/views/admin/Users/ModalUpdateUser/index.tsx
@@ -0,0 +1,71 @@
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import Modal from "@/components/ui/Modal";
+import Select from "@/components/ui/Select";
+import userServices from "@/services/user";
+import { FormEvent, useState } from "react";
+
+const ModalUpdateUser = (props: any) => {
+  const { updatedUser, setUpdatedUser, setUsersData } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const handleUpdateUser = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const form: any = event.target as HTMLFormElement;
+    const data = {
+      role: form.role.value,
+    };
+
+    const result = await userServices.updateUser(updatedUser.id, data);
+
+    if (result.status === 200) {
+      setIsLoading(false);
+      setUpdatedUser({});
+      const { data } = await userServices.getAllUsers();
+      setUsersData(data.data);
+    } else {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Modal onClose={() => setUpdatedUser({})}>
+      <h1>Update User</h1>
+      <form onSubmit={handleUpdateUser}>
+        <Input
+          name="email"
+          label="Email"
+          type="email"
+          defaultValue={updatedUser.email}
+          disabled
+        />
+        <Input
+          name="fullname"
+          label="Fullname"
+          type="text"
+          defaultValue={updatedUser.fullname}
+          disabled
+        />
+        <Input
+          name="phone"
+          label="Phone"
+          type="number"
+          defaultValue={updatedUser.phone}
+          disabled
+        />
+        <Select
+          name="role"
+          label="Role"
+          defaultValue={updatedUser.role}
+          options={[
+            { label: "Admin", value: "admin" },
+            { label: "Member", value: "member" },
+          ]}
+        />
+        <Button type="submit">Update</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default ModalUpdateUser;
--- src/components/views/admin/Users/index.tsx
+++ src/components/views/admin/Users/index.tsx
@@ -1,6 +1,10 @@
 import AdminLayout from "@/components/layouts/AdminLayout";
-import styles from "./Users.module.scss";
 import Button from "@/components/ui/Button";
+import { useEffect, useState } from "react";
+import ModalUpdateUser from "./ModalUpdateUser";
+import styles from "./Users.module.scss";
+import userServices from "@/services/user";
+import ModalDeleteUser from "./ModalDeleteUser";
 
 type PropTypes = {
   users: any;
@@ -8,41 +12,79 @@ type PropTypes = {
 
 const UsersAdminView = (props: PropTypes) => {
   const { users } = props;
+  const [updatedUser, setUpdatedUser] = useState<any>({});
+  const [deletedUser, setDeletedUser] = useState<any>({});
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    setUsersData(users);
+  }, [users]);
+
   return (
-    <AdminLayout>
-      <div className={styles.users}>
-        <h1>User Management</h1>
-        <table className={styles.users__table}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Fullname</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Role</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user: any, index: number) => (
-              <tr key={user.id}>
-                <td>{index + 1}</td>
-                <td>{user.fullname}</td>
-                <td>{user.email}</td>
-                <td>{user.phone}</td>
-                <td>{user.role}</td>
-                <td>
-                  <div className={styles.users__table__action}>
-                    <Button type="button">Update</Button>
-                    <Button type="button">Delete</Button>
-                  </div>
-                </td>
+    <>
+      <AdminLayout>
+        <div className={styles.users}>
+          <h1>User Management</h1>
+          <table className={styles.users__table}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Fullname</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Role</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </AdminLayout>
+            </thead>
+            <tbody>
+              {usersData.map((user: any, index: number) => (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>{user.fullname}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <div className={styles.users__table__action}>
+                      <Button
+                        type="button"
+                        variant="info"
+                        className={styles.users__table__action__edit}
+                        onClick={() => setUpdatedUser(user)}
+                      >
+                        <i className="bx bxs-edit" />
+                      </Button>
+                      <Button
+                        type="button"
+                        variant="danger"
+                        onClick={() => setDeletedUser(user)}
+                      >
+                        <i className="bx bxs-trash-alt" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </AdminLayout>
+      {Object.keys(updatedUser).length && (
+        <ModalUpdateUser
+          updatedUser={updatedUser}
+          setUpdatedUser={setUpdatedUser}
+          setUsersData={setUsersData}
+        />
+      )}
+
+      {Object.keys(deletedUser).length && (
+        <ModalDeleteUser
+          deletedUser={deletedUser}
+          setDeletedUser={setDeletedUser}
+          setUsersData={setUsersData}
+        />
+      )}
+    </>
   );
 };
 
